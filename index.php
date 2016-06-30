303167ad487ed65e2f27ad44afab292696329763
maxvallim <max.vallim@unifeob.edu.br>
2016-06-30 20:45:29 -0300

    retirada da paginação

--- index.php
+++ index.php
@@ -55,17 +55,6 @@
     
           </div>
 
-        <!-- paginação -->
-        <ul class="pagination"> 
-          <li><a href="#">&laquo;</a></li> 
-          <li><a href="#">1</a></li> 
-          <li><a href="#">2</a></li> 
-          <li><a href="#">3</a></li> 
-          <li><a href="#">4</a></li> 
-          <li><a href="#">5</a></li> 
-          <li><a href="#">&raquo;</a></li> 
-        </ul>
-
     </div> <!-- /container -->
 </body>
 </html>
